1bc5cbb2da86887376410731add94d733ff2c058
FMBaton <145272675+FMBaton@users.noreply.github.com>
2023-09-19 13:34:32 +0600

    Update Sou.cpp

--- Sou.cpp
+++ Sou.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
+#include <unordered_set>
 #include <vector>
-#include <algorithm>
 
 int main() {
     int M, N;
-
-    // Шаг 1: Ввод размеров массивов
-    std::cout << "Enter arrey A: ";
+    std::cout << "Enter the size of array A: ";
     std::cin >> M;
 
-    std::cout << "Enter arrey B: ";
+    std::cout << "Enter the size of array B: ";
     std::cin >> N;
 
-    // Шаг 2: Создание массивов A и B
     std::vector<int> A(M);
     std::vector<int> B(N);
+    std::unordered_set<int> commonElements;
 
-    std::cout << "Введите элементы массива A:\n";
-    for (int i = 0; i < M; i++) {
+    std::cout << "Enter elements of array A: ";
+    for (int i = 0; i < M; ++i) {
         std::cin >> A[i];
     }
 
-    std::cout << "Введите элементы массива B:\n";
-    for (int i = 0; i < N; i++) {
+    std::cout << "Enter elements of array B: ";
+    for (int i = 0; i < N; ++i) {
         std::cin >> B[i];
     }
 
-    // Шаг 3: Создание пустого массива C
-    std::vector<int> C;
-
-    // Шаг 4: Нахождение общих элементов без повторений
-    for (int i = 0; i < M; i++) {
-        if (std::find(B.begin(), B.end(), A[i]) != B.end() &&
-            std::find(C.begin(), C.end(), A[i]) == C.end()) {
-            C.push_back(A[i]);
+    // Find common elements and store them in the set
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (A[i] == B[j]) {
+                commonElements.insert(A[i]);
+                break;
+            }
         }
     }
 
-    // Шаг 5: Вывод результата
-    std::cout << "Общие элементы массивов A и B без повторений: ";
-    for (int i = 0; i < C.size(); i++) {
-        std::cout << C[i] << " ";
+    // Convert set to vector for easy printing
+    std::vector<int> C(commonElements.begin(), commonElements.end());
+
+    // Print the common elements
+    std::cout << "Common elements: ";
+    for (int element : C) {
+        std::cout << element << " ";
     }
     std::cout << std::endl;
 
